916c29c3198d357ed6109720c3ebeff9dbc0834c
RiaL <nowaksson@gmail.com>
2012-06-24 13:25:45 +0200

    Dodanie kilku rzeczy

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ActiveRecord::Base
   attr_accessible :category_id, :description, :image_url, :name, :price, :product_number, :rating_number, :rating_sum
+ 
+  default_scope :order => 'name'
+  
+  validates :name, :description, :image_url, :presence => true
+  validates :price, :numericality => {:greatr_than_or_equal_to => 0.01}
+  validates :name, :uniqueness => true
+  validates :image_url, :format => {
+    :with => %r{\.(gif|jpg|png)}i,
+    :message => 'zły typ pliku (.gif, .jpg, .png)'
+  }
 end
