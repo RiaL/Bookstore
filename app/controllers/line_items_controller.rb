554f6ec39cc173be5fdb872903c8afe1666c9420
RiaL <nowaksson@gmail.com>
2012-06-24 17:49:29 +0200

    Przerobki koszyka

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -52,7 +52,8 @@ class LineItemsController < ApplicationController
     
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to(@line_item.cart, :notice => 'Produkt dodano do koszyka!') }
+        format.html { redirect_to(store_url) }
+        format.js {@current_item = @line_item}
         format.xml { render :xml => @line_item, :status => :created, :location => @line_item }
       else
         format.html { render :action => "new" }
